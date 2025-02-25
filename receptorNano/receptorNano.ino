d776faab68869201a1962577cb5bf5fe960a8c64
AyrXZ47 <yovickrz@proton.me>
2025-02-24 19:38:48 -0600

    Aumento en el tiempo de encendido

--- receptorNano/receptorNano.ino
+++ receptorNano/receptorNano.ino
@@ -2,10 +2,10 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-// Pines del nRF24L01+ y el relé 
+// Pines del nRF24L01+ y 2N2222 
 #define CE_PIN 9
 #define CSN_PIN 10
-#define RELAY_PIN 2  // Pin conectado al relé
+#define RELAY_PIN 2  // Pin conectado a la base del 2N2222
 
 // Configuración del módulo RF24
 RF24 radio(CE_PIN, CSN_PIN);
@@ -14,7 +14,7 @@ const byte address[6] = "00001";  // Dirección para la comunicación
 void setup() {
   // Configuración del pin del relé
   pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, LOW);  // Relé apagado al inicio
+  digitalWrite(RELAY_PIN, LOW);  // Apagado al inicio
 
   // Inicialización del radio
   if (!radio.begin()) {
@@ -34,9 +34,9 @@ void loop() {
 
     // Comprobar si el mensaje es "ON"
     if (strcmp(text, "ON") == 0) {
-      digitalWrite(RELAY_PIN, HIGH);  // Activar relé
-      delay(1500);                   // Mantener encendido por 1.5 segundos
-      digitalWrite(RELAY_PIN, LOW);  // Apagar relé
+      digitalWrite(RELAY_PIN, HIGH);  // Activa 2N2222
+      delay(3000);                   // Mantener encendido por 3 segundos
+      digitalWrite(RELAY_PIN, LOW);  // Apaga 2N2222
     }
   }
 }
